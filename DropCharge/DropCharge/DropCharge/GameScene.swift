04861d5c906d89cf43b5eba301175bbce530264b
Tony Cheng <shilmista@gmail.com>
2016-05-08 11:32:39 -0400

    DropCharge sprite kit scenes

--- DropCharge/DropCharge/DropCharge/GameScene.swift
+++ DropCharge/DropCharge/DropCharge/GameScene.swift
@@ -7,39 +7,333 @@
 //
 
 import SpriteKit
+import CoreMotion
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
+    
+    struct PhysicsCategory {
+        static let None: UInt32 = 0
+        static let Player: UInt32 = 0b1
+        static let PlatformNormal: UInt32 = 0b10
+        static let PlatformBreakable: UInt32 = 0b100
+        static let CoinNormal: UInt32 = 0b1000
+        static let CoinSpecial: UInt32 = 0b10000
+        static let Edges:UInt32 = 0b100000
+    }
+    // MARK: - Properties
+    var bgNode = SKNode()
+    var fgNode = SKNode()
+    var background: SKNode!
+    var backHeight: CGFloat = 0.0
+    var player: SKSpriteNode!
+    
+    var platform5Across: SKSpriteNode!
+    var coinArrow: SKSpriteNode!
+    var coinSArrow: SKSpriteNode!
+    var break5Across: SKSpriteNode!
+    var lastItemPosition = CGPointZero
+    var lastItemHeight: CGFloat = 0.0
+    var levelY: CGFloat = 0.0
+    var isPlaying: Bool = false
+    
+    let motionManager = CMMotionManager()
+    var xAcceleration = CGFloat(0)
+    let cameraNode = SKCameraNode()
+    var lava: SKSpriteNode!
+    
+    var lastUpdateTimeInterval: NSTimeInterval = 0
+    var deltaTime: NSTimeInterval = 0
+    
     override func didMoveToView(view: SKView) {
-        /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!"
-        myLabel.fontSize = 45
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
+        setupNodes()
+        setupLevel()
+        setupPlayer()
+        setupCoreMotion()
+        physicsWorld.contactDelegate = self
+        setCameraPosition(CGPoint(x: size.width/2, y: size.height/2))
+    }
+    
+    override func update(currentTime: NSTimeInterval) {
+        // 1
+        if lastUpdateTimeInterval > 0 {
+            deltaTime = currentTime - lastUpdateTimeInterval
+        } else {
+            deltaTime = 0
+        }
+        lastUpdateTimeInterval = currentTime
+        // 2
+        if paused { return }
+        // 3
+        if isPlaying == true {
+            updateCamera()
+            updatePlayer()
+            updateLava(deltaTime)
+            updateCollisionLava()
+            updateLevel()
+        }
+    }
+    
+    func setupNodes() {
+        let worldNode = childNodeWithName("World")!
+        bgNode = worldNode.childNodeWithName("Background")!
+        background = bgNode.childNodeWithName("Overlay")!.copy() as! SKNode
+        backHeight = background.calculateAccumulatedFrame().height
+        fgNode = worldNode.childNodeWithName("Foreground")!
+        player = fgNode.childNodeWithName("Player") as! SKSpriteNode
+        fgNode.childNodeWithName("Bomb")?.runAction(SKAction.hide())
         
-        self.addChild(myLabel)
+        platform5Across = loadOverlayNode("Platform5Across")
+        break5Across = loadOverlayNode("Break5Across")
+        coinArrow = loadOverlayNode("CoinArrow")
+        coinSArrow = loadOverlayNode("CoinSArrow")
+        
+        lava = fgNode.childNodeWithName("Lava") as! SKSpriteNode
+        
+        addChild(cameraNode)
+        camera = cameraNode
+    }
+    
+    func setupLevel() {
+        let initialPlatform = platform5Across.copy() as! SKSpriteNode
+        var itemPosition = player.position
+        itemPosition.y = player.position.y -
+            ((player.size.height * 0.5) +
+                (initialPlatform.size.height * 0.20))
+        initialPlatform.position = itemPosition
+        fgNode.addChild(initialPlatform)
+        lastItemPosition = itemPosition
+        lastItemHeight = initialPlatform.size.height / 2.0
+        
+        levelY = bgNode.childNodeWithName("Overlay")!.position.y + backHeight
+        while lastItemPosition.y < levelY {
+            addRandomOverlayNode()
+        }
+    }
+    
+    func setupPlayer() {
+        player.physicsBody = SKPhysicsBody(circleOfRadius: player.size.width * 0.3)
+        player.physicsBody!.dynamic = false
+        player.physicsBody!.allowsRotation = false
+        player.physicsBody!.categoryBitMask = PhysicsCategory.Player
+        player.physicsBody!.collisionBitMask = 0
+    }
+    
+    func setupCoreMotion() {
+        motionManager.accelerometerUpdateInterval = 0.2
+        let queue = NSOperationQueue()
+        motionManager.startAccelerometerUpdatesToQueue(queue, withHandler: {
+            accelerometerData, error in
+            guard let accelerometerData = accelerometerData else {
+                return
+            }
+            let acceleration = accelerometerData.acceleration
+            self.xAcceleration = (CGFloat(acceleration.x) * 0.75) + (self.xAcceleration * 0.25)
+        })
+    }
+    
+    func loadOverlayNode(fileName: String) -> SKSpriteNode {
+        let overlayScene = SKScene(fileNamed: fileName)!
+        let contentTemplateNode =
+            overlayScene.childNodeWithName("Overlay")
+        return contentTemplateNode as! SKSpriteNode
+    }
+    func createOverlayNode(nodeType: SKSpriteNode, flipX: Bool) {
+        let platform = nodeType.copy() as! SKSpriteNode
+        lastItemPosition.y = lastItemPosition.y +
+            (lastItemHeight + (platform.size.height / 2.0))
+        lastItemHeight = platform.size.height / 2.0
+        platform.position = lastItemPosition
+        if flipX == true {
+            platform.xScale = -1.0
+        }
+        fgNode.addChild(platform)
+    }
+    
+    func addRandomOverlayNode() {
+        let overlaySprite: SKSpriteNode!
+        let platformPercentage = 60
+        if Int.random(min: 1, max: 100) <= platformPercentage {
+            let breakablePercentage = 25
+            if Int.random(min: 1, max: 100) <= breakablePercentage {
+                overlaySprite = break5Across
+            }
+            else {
+                overlaySprite = platform5Across
+            }
+        }
+        else {
+            let specialPercentage = 25
+            if Int.random(min: 1, max: 100) <= specialPercentage {
+                overlaySprite = coinSArrow
+            }
+            else {
+                overlaySprite = coinArrow
+            }
+        }
+        createOverlayNode(overlaySprite, flipX: false)
+    }
+    
+    func createBackgroundNode() {
+        let backNode = background.copy() as! SKNode
+        backNode.position = CGPoint(x: 0.0, y: levelY)
+        bgNode.addChild(backNode)
+        levelY += backHeight
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-       /* Called when a touch begins */
-        
-        for touch in touches {
-            let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
+        if !isPlaying {
+            bombDrop()
         }
     }
-   
-    override func update(currentTime: CFTimeInterval) {
-        /* Called before each frame is rendered */
+    
+    func bombDrop() {
+        let scaleUp = SKAction.scaleTo(1.25, duration: 0.25)
+        let scaleDown = SKAction.scaleTo(1.0, duration: 0.25)
+        let sequence = SKAction.sequence([scaleUp, scaleDown])
+        let repeatSeq = SKAction.repeatActionForever(sequence)
+        fgNode.childNodeWithName("Bomb")!.runAction(SKAction.unhide())
+        fgNode.childNodeWithName("Bomb")!.runAction(repeatSeq)
+        runAction(SKAction.sequence([SKAction.waitForDuration(2),
+            SKAction.runBlock(startGame)
+            ]))
     }
+    
+    func startGame() {
+        fgNode.childNodeWithName("Title")!.removeFromParent()
+        fgNode.childNodeWithName("Bomb")!.removeFromParent()
+        isPlaying = true
+        player.physicsBody!.dynamic = true
+        superBoostPlayer()
+    }
+    
+    func setPlayerVelocity(amount:CGFloat) {
+        let gain: CGFloat = 2.5
+        player.physicsBody!.velocity.dy =
+            max(player.physicsBody!.velocity.dy, amount * gain)
+    }
+    func jumpPlayer() {
+        setPlayerVelocity(650)
+    }
+    func boostPlayer() {
+        setPlayerVelocity(1200)
+    }
+    func superBoostPlayer() {
+        setPlayerVelocity(1700)
+    }
+    
+    func didBeginContact(contact: SKPhysicsContact) {
+        let other = contact.bodyA.categoryBitMask == PhysicsCategory.Player ? contact.bodyB : contact.bodyA
+        switch other.categoryBitMask {
+        case PhysicsCategory.CoinNormal:
+            if  let coin = other.node as? SKSpriteNode {
+                coin.removeFromParent()
+                jumpPlayer()
+            }
+        case PhysicsCategory.CoinSpecial:
+            if let coin = other.node as? SKSpriteNode {
+                coin.removeFromParent()
+                boostPlayer()
+            }
+        case PhysicsCategory.PlatformNormal:
+            if let _ = other.node as? SKSpriteNode {
+                if player.physicsBody!.velocity.dy < 0 {
+                    jumpPlayer()
+                }
+            }
+        case PhysicsCategory.PlatformBreakable:
+            if let platform = other.node as? SKSpriteNode {
+                if player.physicsBody!.velocity.dy < 0 {
+                    jumpPlayer()
+                    platform.removeFromParent()
+                }
+            }
+        default:
+            break;
+        }
+    }
+    
+    func updateLevel() {
+        let cameraPos = getCameraPosition()
+        if cameraPos.y > levelY - (size.height * 0.55) {
+            createBackgroundNode()
+            while lastItemPosition.y < levelY {
+                addRandomOverlayNode()
+            }
+        }
+    }
+    
+    func updatePlayer() {
+        player.physicsBody?.velocity.dx = xAcceleration * 1000.0
+        
+        var playerPosition = convertPoint(player.position,
+                                          fromNode: fgNode)
+        if playerPosition.x < -player.size.width/2 {
+            playerPosition = convertPoint(CGPoint(x: size.width +
+                player.size.width/2, y: 0.0), toNode: fgNode)
+            player.position.x = playerPosition.x
+        }
+        else if playerPosition.x > size.width + player.size.width/2 {
+            playerPosition = convertPoint(CGPoint(x:
+                -player.size.width/2, y: 0.0), toNode: fgNode)
+            player.position.x = playerPosition.x
+        }
+    }
+    
+    func overlapAmount() -> CGFloat {
+        guard let view = self.view else {
+            return 0
+        }
+        let scale = view.bounds.size.height / self.size.height
+        let scaledWidth = self.size.width * scale
+        let scaledOverlap = scaledWidth - view.bounds.size.width
+        return scaledOverlap / scale
+    }
+    
+    func getCameraPosition() -> CGPoint {
+        return CGPoint(
+            x: cameraNode.position.x + overlapAmount()/2,
+            y: cameraNode.position.y)
+    }
+    func setCameraPosition(position: CGPoint) {
+        cameraNode.position = CGPoint(
+            x: position.x - overlapAmount()/2,
+            y: position.y)
+    }
+    
+    func updateCamera() {
+        let cameraTarget = convertPoint(player.position, fromNode: fgNode)
+        
+        var targetPosition = CGPoint(x: getCameraPosition().x, y: cameraTarget.y - (scene!.view!.bounds.height * 0.4))
+        let lavaPos = convertPoint(lava.position, fromNode: fgNode)
+        targetPosition.y = max(targetPosition.y, lavaPos.y)
+        
+        let diff  = targetPosition - getCameraPosition()
+        
+        let lerpValue = CGFloat(0.2)
+        let lerpDiff = diff * lerpValue
+        let newPosition = getCameraPosition() + lerpDiff
+        
+        setCameraPosition(CGPoint(x: size.width/2, y: newPosition.y))
+        
+    }
+    
+    func updateLava(dt: NSTimeInterval) {
+        let lowerLeft = CGPoint(x: 0, y: cameraNode.position.y - (size.height/2))
+        let visibleMinYFg = scene!.convertPoint(lowerLeft, toNode: fgNode).y
+        let lavaVelocity = CGPoint(x: 0, y: 120)
+        let lavaStep = lavaVelocity * CGFloat(dt)
+        var newPosition = lava.position + lavaStep
+        // 4
+        newPosition.y = max(newPosition.y, (visibleMinYFg - 125.0))
+        // 5
+        lava.position = newPosition
+    }
+    
+    func updateCollisionLava() {
+        if player.position.y < lava.position.y + 90 {
+            boostPlayer()
+        }
+    }
+    
+    
 }
